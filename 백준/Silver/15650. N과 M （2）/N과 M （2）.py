e9b115c6e437cb101787f406598cb9ea0e58310a
SuIn Roh <bigpie1367@gmail.com>
2024-10-10 13:42:05 +0900

    [Silver III] Title: N과 M (2), Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.py
@@ -0,0 +1,33 @@
+import sys
+
+N, M = map(int, sys.stdin.readline().split())
+
+visited = [False] * (N + 1)
+ans = []
+
+
+def dfs(depth):
+    global ans
+
+    if depth == M:
+        print(*ans)
+        return
+
+    for i in range(1, N+1):
+        if visited[i]:
+            continue
+
+        if depth > 0:
+            if i < ans[depth - 1]:
+                continue
+
+        ans.append(i)
+        visited[i] = True
+
+        dfs(depth + 1)
+
+        ans.pop()
+        visited[i] = False
+
+
+dfs(0)
